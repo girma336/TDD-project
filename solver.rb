664c3e6ece0752c6fe43484dd61e83b3e42298e6
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-23 23:56:05 +0430

    Add factorial method

---
+++ solver.rb
@@ -0,0 +1,11 @@
+class Solver
+  def factorial(number)
+    raise 'Negative numbers not allowed' if number.negative?
+    return 1 if [0, 1].include?(number)
+
+    number * factorial(number - 1)
+  end
+
+  def reverse_string(string)
+    string.reverse
+  end
